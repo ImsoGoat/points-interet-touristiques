fb9ac74592e576aa5e4dfa432b6da9fa19d1a9d8
Annen Julien <julien.annen@he-arc.ch>
2025-01-22 16:01:07 +0100

    initialisation du projet

---
+++ points-interet-touristiques/src/main/java/ch/hearc/jee_project/pointsinterettouristiques/PointsInteretTouristiquesApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.jee_project.pointsinterettouristiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsInteretTouristiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointsInteretTouristiquesApplication.class, args);
+	}
+
+}
---
+++ points-interet-touristiques/src/test/java/ch/hearc/jee_project/pointsinterettouristiques/PointsInteretTouristiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hearc.jee_project.pointsinterettouristiques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsInteretTouristiquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
